264253cf08d9977c5aff5d290a06dd67a9e90fc4
GabrielTMangum <gabrieltmangum@gmail.com>
2019-10-28 01:37:35 -0400

    forgot to save my script file before pushing.

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,61 +6,52 @@ console.log(time)
 $("#date").text(moment().format('dddd, MMMM Do YYYY'))
 
 
-$("#nine").ready(timeChecker (9))
-$("#nineBtn").click(saveNotes (9))
+$("#nine").ready(timeChecker(9))
+$("#nineBtn").click(saveNotes(9))
 
-$("#ten").ready(timeChecker (10))
-$("#tenBtn").click(saveNotes (10))
+$("#ten").ready(timeChecker(10))
+$("#tenBtn").click(saveNotes(10))
 
-$("#elven").ready(timeChecker (11))
-$("#elevenBtn").click(saveNotes (11))
+$("#elven").ready(timeChecker(11))
+$("#elevenBtn").click(saveNotes(11))
 
-$("#twelve").ready(timeChecker (12))
-$("#twelveBtn").click(saveNotes (12))
+$("#twelve").ready(timeChecker(12))
+$("#twelveBtn").click(saveNotes(12))
 
-$("#thirteen").ready(timeChecker (13))
-$("#thirteenBtn").click(saveNotes (13))
+$("#thirteen").ready(timeChecker(13))
+$("#thirteenBtn").click(saveNotes(13))
 
-$("#fourteen").ready(timeChecker (14))
-$("#fourteenBtn").click(saveNotes (14))
+$("#fourteen").ready(timeChecker(14))
+$("#fourteenBtn").click(saveNotes(14))
 
-$("#fifteenteen").ready(timeChecker (15))
-$("#fifteenBtn").click(saveNotes (15))
+$("#fifteenteen").ready(timeChecker(15))
+$("#fifteenBtn").click(saveNotes(15))
 
-$("#sixteen").ready(timeChecker (16))
-$("#sixteenBtn").click(saveNotes (16))
+$("#sixteen").ready(timeChecker(16))
+$("#sixteenBtn").click(saveNotes(16))
 
-$("#seventeen").ready(timeChecker (17))
-$("#seventeenBtn").click(saveNotes (17))
+$("#seventeen").ready(timeChecker(17))
+$("#seventeenBtn").click(saveNotes(17))
 
-$("#eightteen").ready(timeChecker (18))
-$("#eightteenBtn").click(saveNotes (18))
+$("#eightteen").ready(timeChecker(18))
+$("#eightteenBtn").click(saveNotes(18))
 
 
 
-function timeChecker (x) {
+function timeChecker(x) {
     if (x > time) {
-        $("#" + x).css("background-color","#a347ff")
+        $("#" + x).css("background-color", "#a347ff")
     } else if (x < time) {
-        $("#" + x).css("background-color","#fcb18d")
-    } else if (x === time){
-        $("#" + x).css("background-color","#ff5447")
+        $("#" + x).css("background-color", "#fcb18d")
+    } else if (x === time) {
+        $("#" + x).css("background-color", "#ff5447")
     }
 }
 
 
-function saveNotes (x) {
-        localStorage.setItem(x + "Notes", $("#" + x).val())  
+function saveNotes(x) {
+    localStorage.setItem(x + "Notes", $("#" + x).val())
 
-    
-        }; 
 
-// $('#load').on('click', function(){
-//     $('input[type="text"]').each(function(){    
-//         var id = $(this).attr('id');
-//         var value = localStorage.getItem(id);
+};
 
-//         $(this).val(value);
-
-//     }); 
-// });
