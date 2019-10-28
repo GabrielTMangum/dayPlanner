460a8a87a49c0a9dafb23968b5137b5a7dc2c08b
GabrielTMangum <gabrieltmangum@gmail.com>
2019-10-28 01:36:57 -0400

    I added the rest of the time slots back in, I can't figure out the local storage stuff so I'll have to get help with that tomorrow.

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,36 @@ $("#date").text(moment().format('dddd, MMMM Do YYYY'))
 
 
 $("#nine").ready(timeChecker (9))
-$("#nineBtn").click(saveNotes("nine"))
+$("#nineBtn").click(saveNotes (9))
+
+$("#ten").ready(timeChecker (10))
+$("#tenBtn").click(saveNotes (10))
+
+$("#elven").ready(timeChecker (11))
+$("#elevenBtn").click(saveNotes (11))
+
+$("#twelve").ready(timeChecker (12))
+$("#twelveBtn").click(saveNotes (12))
+
+$("#thirteen").ready(timeChecker (13))
+$("#thirteenBtn").click(saveNotes (13))
+
+$("#fourteen").ready(timeChecker (14))
+$("#fourteenBtn").click(saveNotes (14))
+
+$("#fifteenteen").ready(timeChecker (15))
+$("#fifteenBtn").click(saveNotes (15))
+
+$("#sixteen").ready(timeChecker (16))
+$("#sixteenBtn").click(saveNotes (16))
+
+$("#seventeen").ready(timeChecker (17))
+$("#seventeenBtn").click(saveNotes (17))
+
+$("#eightteen").ready(timeChecker (18))
+$("#eightteenBtn").click(saveNotes (18))
+
+
 
 function timeChecker (x) {
     if (x > time) {
@@ -21,5 +50,17 @@ function timeChecker (x) {
 
 
 function saveNotes (x) {
-    console.log(x)
-}
+        localStorage.setItem(x + "Notes", $("#" + x).val())  
+
+    
+        }; 
+
+// $('#load').on('click', function(){
+//     $('input[type="text"]').each(function(){    
+//         var id = $(this).attr('id');
+//         var value = localStorage.getItem(id);
+
+//         $(this).val(value);
+
+//     }); 
+// });
